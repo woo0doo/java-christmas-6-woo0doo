858fcbcab9ba22e16653619a956bc3680dcef4d9
WooYoungDoo <duden5000@naver.com>
2023-11-10 19:39:42 +0900

    feat (InputView) : 입력 관련 기능

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,14 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public String inputDateOfVisit() {
+        return Console.readLine();
+    }
+
+    public String inputMenusAndCounts() {
+        return Console.readLine();
+    }
+}
