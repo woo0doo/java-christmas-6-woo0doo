147207aeb173cf029b7b49be228cec219bd2675d
WooYoungDoo <duden5000@naver.com>
2023-11-11 16:00:43 +0900

    feat (Person) : 이벤트 진행하는 사람 객체 기능

---
+++ src/main/java/christmas/model/Person.java
@@ -0,0 +1,32 @@
+package christmas.model;
+
+import java.util.Map;
+
+import static christmas.constant.ErrorConstant.ERROR_PREFIX;
+import static christmas.constant.ErrorConstant.OVER_MAX_COUNT_SIZE_ERROR_MESSAGE;
+
+public class Person {
+
+    Map<Menu, Integer> menuAndCount;
+    int totalCount;
+    int totalOrderPriceBeforeDiscount;
+
+    private static final int MAX_COUNT_SIZE = 20;
+
+    public void setMenuAndCount(Map<Menu, Integer> menuAndCount) {
+        this.menuAndCount = menuAndCount;
+    }
+
+    public void setTotalCount(int totalCount) {
+        validateTotalCountOverMaxCount(totalCount);
+        this.totalCount = totalCount;
+    }
+
+    public void setTotalOrderPriceBeforeDiscount(int totalOrderPriceBeforeDiscount) {
+        this.totalOrderPriceBeforeDiscount = totalOrderPriceBeforeDiscount;
+    }
+
+    private void validateTotalCountOverMaxCount(int totalCount) {
+        if (totalCount > MAX_COUNT_SIZE) throw new IllegalArgumentException(ERROR_PREFIX + OVER_MAX_COUNT_SIZE_ERROR_MESSAGE);
+    }
+}
