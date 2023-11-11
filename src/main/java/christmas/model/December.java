222a4bc79d358e5c36b1616f7127c71a2d3ce4f0
WooYoungDoo <duden5000@naver.com>
2023-11-11 20:20:49 +0900

    feat (December) : 12월 주중, 주말, 특별 날 구분 enum

---
+++ src/main/java/christmas/model/December.java
@@ -0,0 +1,29 @@
+package christmas.model;
+
+import java.util.List;
+
+public enum December {
+
+    WEEKDAY(List.of(3,4,5,6,7,10,11,12,13,14,17,18,19,20,21,24,25,26,27,28,31)),
+    WEEKEND(List.of(1,2,8,9,15,16,22,23,29,30)),
+    STAR_DAY(List.of(3,10,17,24,25,31));
+
+    December(List<Integer> day) {
+        this.day = day;
+    }
+
+    private final List<Integer> day;
+
+    public static boolean containDayInWeekday(int dateOfVisit) {
+        return WEEKDAY.day.contains(dateOfVisit);
+    }
+
+    public static boolean containDayInWeekend(int dateOfVisit) {
+        return WEEKEND.day.contains(dateOfVisit);
+    }
+
+    public static boolean containDayInStarDay(int dateofVisit) {
+        return STAR_DAY.day.contains(dateofVisit);
+    }
+
+}
