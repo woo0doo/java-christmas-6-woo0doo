19532fa7717d208007033b26031da8947317db46
WooYoungDoo <duden5000@naver.com>
2023-11-11 13:44:39 +0900

    feat (ValidateService) : 입력 메뉴와 개수 관련 입출력 및 검증

--- src/main/java/christmas/controller/EventController.java
+++ src/main/java/christmas/controller/EventController.java
@@ -10,8 +10,31 @@ public class EventController {
     private final OutputView outputView = new OutputView();
     private final ValidateService validateService = new ValidateService();
 
+    private String inputDateOfVisit;
+    private String inputMenusAndCounts;
+
     public void run() {
         startAndDateOfVisitProcess();
+        MenusAndCountsProcess();
+    }
+
+    private void MenusAndCountsProcess() {
+        printAskOrderMenusAndCounts();
+        inputOrdersAndCounts();
+    }
+
+    private void inputOrdersAndCounts() {
+        try {
+            inputMenusAndCounts = inputView.inputMenusAndCounts();
+            validateService.validateInputMenusAndCounts(inputMenusAndCounts);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            inputOrdersAndCounts();
+        }
+    }
+
+    private void printAskOrderMenusAndCounts() {
+        outputView.printAskOrderMenusAndCounts();
     }
 
     private void startAndDateOfVisitProcess() {
@@ -25,7 +48,7 @@ public class EventController {
 
     private void inputDateOfVisit() {
         try {
-            String inputDateOfVisit = inputView.inputDateOfVisit();
+            inputDateOfVisit = inputView.inputDateOfVisit();
             validateService.validateInputDateOfVisit(inputDateOfVisit);
 
         } catch (IllegalArgumentException e) {
