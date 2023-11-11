a53ce4c872e313086014b867b9835edeec72b7ba
WooYoungDoo <duden5000@naver.com>
2023-11-11 16:39:26 +0900

    refactor (Menu) : 메뉴 한글 이름 enum 속성 추가

--- src/main/java/christmas/model/Menu.java
+++ src/main/java/christmas/model/Menu.java
@@ -4,19 +4,23 @@ import static christmas.constant.ErrorConstant.ERROR_PREFIX;
 
 public enum Menu {
 
-    BUTTON_MUSHROOM_SOUP(Course.APPETIZER, 6_000),
-    TAPAS(Course.APPETIZER, 5_500),
-    CAESAR_SALAD(Course.APPETIZER, 8_000),
+    BUTTON_MUSHROOM_SOUP("양송이수프", Course.APPETIZER, 6_000),
+    TAPAS("타파스", Course.APPETIZER, 5_500),
+    CAESAR_SALAD("시저샐러드", Course.APPETIZER, 8_000),
 
-    T_BONE_STEAK(Course.MAIN, 55_000),
-    BBQ_RIB(Course.MAIN, 54_000),
-    SEAFOOD_PASTA(Course.MAIN, 35_000),
-    CHRISTMAS_PASTA(Course.MAIN, 25_000),
-    CHOCOLATE_CAKE(Course.DESSERT, 15_000),
-    ICECREAM(Course.DESSERT, 5_000),
-    ZERO_COKE(Course.BEVERAGE, 3_000),
-    RED_WINE(Course.BEVERAGE, 60_000),
-    CHAMPAGNE(Course.BEVERAGE, 25_000);
+    T_BONE_STEAK("티본스테이크", Course.MAIN, 55_000),
+    BBQ_RIB("바비큐립", Course.MAIN, 54_000),
+    SEAFOOD_PASTA("해산물파스타", Course.MAIN, 35_000),
+    CHRISTMAS_PASTA("크리스마스파스타", Course.MAIN, 25_000),
+    CHOCOLATE_CAKE("초코케이크", Course.DESSERT, 15_000),
+    ICECREAM("아이스크림", Course.DESSERT, 5_000),
+    ZERO_COKE("제로콜라", Course.BEVERAGE, 3_000),
+    RED_WINE("레드와인", Course.BEVERAGE, 60_000),
+    CHAMPAGNE("샴페인", Course.BEVERAGE, 25_000);
+
+    public String getMenuKoreaName() {
+        return menuKoreaName;
+    }
 
     public Course getCourse() {
         return course;
@@ -33,29 +37,31 @@ public enum Menu {
         BEVERAGE
     }
 
-    Menu(Course course, int price) {
+    Menu(String menuKoreaName, Course course, int price) {
+        this.menuKoreaName = menuKoreaName;
         this.course = course;
         this.price = price;
     }
 
+    private final String menuKoreaName;
     private final Course course;
     private final int price;
 
     private static final String VALIDATE_CORRECT_MENU_ERROR_MESSAGE = "유효하지 않은 주문입니다. 다시 입력해 주세요.";
 
     public static Menu StringToEnum(String menu) {
-        if (menu.equals("양송이수프")) return BUTTON_MUSHROOM_SOUP;
-        else if (menu.equals("타파스")) return TAPAS;
-        else if (menu.equals("시저샐러드")) return CAESAR_SALAD;
-        else if (menu.equals("티본스테이크")) return T_BONE_STEAK;
-        else if (menu.equals("바비큐립")) return BBQ_RIB;
-        else if (menu.equals("해산물파스타")) return SEAFOOD_PASTA;
-        else if (menu.equals("크리스마스파스타")) return CHRISTMAS_PASTA;
-        else if (menu.equals("초코케이크")) return CHOCOLATE_CAKE;
-        else if (menu.equals("아이스크림")) return ICECREAM;
-        else if (menu.equals("제로콜라")) return ZERO_COKE;
-        else if (menu.equals("레드와인")) return RED_WINE;
-        else if (menu.equals("샴페인")) return CHAMPAGNE;
+        if (menu.equals(BUTTON_MUSHROOM_SOUP.menuKoreaName)) return BUTTON_MUSHROOM_SOUP;
+        else if (menu.equals(TAPAS.menuKoreaName)) return TAPAS;
+        else if (menu.equals(CAESAR_SALAD.menuKoreaName)) return CAESAR_SALAD;
+        else if (menu.equals(T_BONE_STEAK.menuKoreaName)) return T_BONE_STEAK;
+        else if (menu.equals(BBQ_RIB.menuKoreaName)) return BBQ_RIB;
+        else if (menu.equals(SEAFOOD_PASTA.menuKoreaName)) return SEAFOOD_PASTA;
+        else if (menu.equals(CHRISTMAS_PASTA.menuKoreaName)) return CHRISTMAS_PASTA;
+        else if (menu.equals(CHOCOLATE_CAKE.menuKoreaName)) return CHOCOLATE_CAKE;
+        else if (menu.equals(ICECREAM.menuKoreaName)) return ICECREAM;
+        else if (menu.equals(ZERO_COKE.menuKoreaName)) return ZERO_COKE;
+        else if (menu.equals(RED_WINE.menuKoreaName)) return RED_WINE;
+        else if (menu.equals(CHAMPAGNE.menuKoreaName)) return CHAMPAGNE;
         throw new IllegalArgumentException(ERROR_PREFIX + VALIDATE_CORRECT_MENU_ERROR_MESSAGE);
     }
 }
