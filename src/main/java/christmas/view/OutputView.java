acc9444a1b99442ae23683007dfe057a727f1d02
WooYoungDoo <duden5000@naver.com>
2023-11-11 16:40:20 +0900

    feat (OutputView) : 주문 메뉴 출력 기능

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,10 +1,19 @@
 package christmas.view;
 
+import christmas.model.Menu;
+
+import java.util.List;
+import java.util.Map;
+
 public class OutputView {
 
     private static final String INTRODUCE_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private static final String ASK_DATE_OF_VISIT_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
-    private static final String ASK_ORDER_MENUS_AND_COUNTS = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+    private static final String ASK_ORDER_MENUS_AND_COUNTS_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+    private static final String EVENT_BENEFIT_PREVIEW_MESSAGE = "12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+    private static final String BLANK_LINE = "";
+    private static final String ORDER_MENU_MESSAGE = "<주문 메뉴>";
+    private static final String ORDER_MENU_AND_COUNT = "%s %d개\n";
 
     public void printStartMessage() {
         printIntroduceMessage();
@@ -12,7 +21,24 @@ public class OutputView {
     }
 
     public void printAskOrderMenusAndCounts() {
-        System.out.println(ASK_ORDER_MENUS_AND_COUNTS);
+        System.out.println(ASK_ORDER_MENUS_AND_COUNTS_MESSAGE);
+    }
+
+    public void printEventBenefitPreviewMessage() {
+        System.out.println(EVENT_BENEFIT_PREVIEW_MESSAGE);
+        printBlankLine();
+    }
+
+    public void printOrderMenus(List<Menu> menus, Map<Menu, Integer> menusAndCounts) {
+        System.out.println(ORDER_MENU_MESSAGE);
+        for (Menu menu : menus) {
+            System.out.printf(ORDER_MENU_AND_COUNT, menu.getMenuKoreaName(), menusAndCounts.get(menu));
+        }
+        System.out.println(ORDER_MENU_MESSAGE);
+    }
+
+    private void printBlankLine() {
+        System.out.println(BLANK_LINE);
     }
 
     private void printIntroduceMessage() {
