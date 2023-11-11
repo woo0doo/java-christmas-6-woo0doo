28df78f80e24881d1b8a7514b6d0e2648b591113
WooYoungDoo <duden5000@naver.com>
2023-11-11 13:43:55 +0900

    feat (InputValidator) : 입력 메뉴와 개수 검증 기능

--- src/main/java/christmas/validator/InputValidator.java
+++ src/main/java/christmas/validator/InputValidator.java
@@ -1,26 +1,43 @@
 package christmas.validator;
 
+import christmas.model.Menu;
+
+import java.util.List;
 import java.util.regex.Pattern;
 
 import static christmas.constant.ErrorConstant.ERROR_PREFIX;
+import static christmas.util.SeparationUtil.separateMenus;
+import static christmas.util.SeparationUtil.separateMenusAndCount;
 
 public class InputValidator {
     private static final String VALIDATE_DATE_OF_VISIT_ERROR_MESSAGE = "유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    private static final String VALIDATE_CORRECT_MENU_ERROR_MESSAGE = "유효하지 않은 주문입니다. 다시 입력해 주세요.";
     private static final String DIGIT_VALIDATE_REGEX = "-?[0-9]+";
     private static final int START_DATE_RANGE = 1;
     private static final int END_DATE_RANGE = 31;
 
     public void validateDateOfVisit(String inputDateOfVisit) {
-        if (isNotDigitOrNotCorrectRangeOfDate(inputDateOfVisit)) {
+        if (isNotDigit(inputDateOfVisit) || isNotCorrectRangeOfDate(inputDateOfVisit)) {
             throw new IllegalArgumentException(ERROR_PREFIX + VALIDATE_DATE_OF_VISIT_ERROR_MESSAGE);
         }
     }
 
-    private boolean isNotDigitOrNotCorrectRangeOfDate(String inputDateOfVisit) {
-        if (!Pattern.compile(DIGIT_VALIDATE_REGEX).matcher(inputDateOfVisit).matches()) {
-            return true;
+    public void validateMenusAndCounts(String inputMenusAndCounts) {
+        List<String> menusAndCounts = separateMenus(inputMenusAndCounts);
+        for (String menusAndCount : menusAndCounts) {
+            List<String> separateMenusAndCount = separateMenusAndCount(menusAndCount);
+            Menu.StringToEnum(separateMenusAndCount.get(0));
+            if (isNotDigit(separateMenusAndCount.get(1))) {
+                throw new IllegalArgumentException(ERROR_PREFIX + VALIDATE_CORRECT_MENU_ERROR_MESSAGE);
+            }
         }
+    }
 
+    private boolean isNotDigit(String inputString) {
+        return !Pattern.compile(DIGIT_VALIDATE_REGEX).matcher(inputString).matches();
+    }
+
+    private boolean isNotCorrectRangeOfDate(String inputDateOfVisit) {
         int dateOfVisit = Integer.parseInt(inputDateOfVisit);
         return dateOfVisit < START_DATE_RANGE || dateOfVisit > END_DATE_RANGE;
     }
