85963ee34ee3d185b749a253635310737c70051d
WooYoungDoo <duden5000@naver.com>
2023-11-11 20:23:32 +0900

    feat (EventService) : 샴페인 증정 여부 및 날짜/주문에 따른 할인 세부 메세지 생성

--- src/main/java/christmas/service/EventService.java
+++ src/main/java/christmas/service/EventService.java
@@ -1,5 +1,6 @@
 package christmas.service;
 
+import christmas.model.December;
 import christmas.model.Menu;
 import christmas.model.Menu.Course;
 import christmas.model.Person;
@@ -10,14 +11,23 @@ import java.util.List;
 import java.util.Map;
 
 import static christmas.constant.ErrorConstant.*;
+import static christmas.util.ConvertUtil.convertIntPriceToStringCommaPrice;
 import static christmas.util.SeparationUtil.separateMenus;
 import static christmas.util.SeparationUtil.separateMenusAndCount;
 
 public class EventService {
 
+    private static final int BASED_ON_OVER_PRICE_GIVE_GIFT_MENU = 120_000;
+    private static final String ONE_CHAMPAGNE_MESSAGE = "샴페인 1개";
+    private static final String NO_GIFT_MESSAGE = "없음";
+    private static final int STAR_DAY_DISCOUNT_PRICE = 1000;
+    private static final int CHAMPAGNE_PRICE = 25000;
 
     private final Person person = new Person();
     int totalCount = 0;
+    int totalDiscountPrice = 0;
+    List<String> discountDetailsMessage = new ArrayList<>();
+
     boolean isNotOnlyBeverage = false;
     private List<Menu> menus = new ArrayList<>();
 
@@ -42,16 +52,20 @@ public class EventService {
 
     public String getStringCommaTotalOrderPriceBeforeDiscount() {
         int totalOrderPriceBeforeDiscount = getTotalOrderPriceBeforeDiscount();
-        return convertIntTotalPriceToStringCommaPrice(totalOrderPriceBeforeDiscount);
+        return convertIntPriceToStringCommaPrice(totalOrderPriceBeforeDiscount);
+    }
+
+    public String getGiftMenu() {
+        if (person.getTotalOrderPriceBeforeDiscount() > BASED_ON_OVER_PRICE_GIVE_GIFT_MENU) {
+            person.setPresentedChampagne(true);
+            return ONE_CHAMPAGNE_MESSAGE;
+        }
+        person.setPresentedChampagne(false);
+        return NO_GIFT_MESSAGE;
     }
 
     private int getTotalOrderPriceBeforeDiscount() {
         return person.getTotalOrderPriceBeforeDiscount();
-    }
-
-    private String convertIntTotalPriceToStringCommaPrice(int totalOrderPriceBeforeDiscount) {
-        return String.format("%,d", totalOrderPriceBeforeDiscount);
-
     }
 
     private Map<Menu, Integer> resolveInputMenusAndCounts(String inputMenusAndCounts) {
@@ -96,10 +110,110 @@ public class EventService {
             throw new IllegalArgumentException(ERROR_PREFIX + ONLY_BEVERAGE_ERROR_MESSAGE);
     }
 
+    public void setDateOfVisit(String inputDateofVisit) {
+        int dateOfVisit = Integer.parseInt(inputDateofVisit);
+        person.setDateOfVisit(dateOfVisit);
+    }
+
+    public int getDateOfVisit() {
+        return person.getDateOfVisit();
+    }
 
     private void setInitPerson(int totalCount, Map<Menu, Integer> menusAndCounts, int totalOrderPriceBeforeDiscount) {
         person.setTotalCount(totalCount);
         person.setMenuAndCount(menusAndCounts);
         person.setTotalOrderPriceBeforeDiscount(totalOrderPriceBeforeDiscount);
+    }
+
+    public List<String> getDiscountDetails() {
+        List<Menu> menus = getMenus();
+        int dateOfVisit = getDateOfVisit();
+        Map<Menu, Integer> menusAndCounts = getPersonMenusAndCounts();
+        addChristmasD_DayDiscountMessage(discountChristmasD_Day(dateOfVisit));
+        discountWeekday(menusAndCounts, menus, dateOfVisit);
+        discountWeekend(menusAndCounts, menus, dateOfVisit);
+        discountStarDay(dateOfVisit);
+        discountGiftEvent(person);
+        person.setTotalDiscountPrice(totalDiscountPrice);
+        return discountDetailsMessage;
+    }
+
+    private void discountGiftEvent(Person person) {
+        if (!person.getIsPresentedChampagne())
+            return;
+
+        totalDiscountPrice += CHAMPAGNE_PRICE;
+        String stringCommaChampagneDiscountPrice = convertIntPriceToStringCommaPrice(CHAMPAGNE_PRICE);
+        discountDetailsMessage.add("증정 이벤트: -" + stringCommaChampagneDiscountPrice + "원");
+    }
+
+    private void discountStarDay(int dateOfVisit) {
+        if (!December.containDayInStarDay(dateOfVisit))
+            return;
+
+        totalDiscountPrice += STAR_DAY_DISCOUNT_PRICE;
+        String stringCommaStarDayDiscountPrice = convertIntPriceToStringCommaPrice(STAR_DAY_DISCOUNT_PRICE);
+        addDiscountPriceMessage("특별", stringCommaStarDayDiscountPrice);
+    }
+
+    private void discountWeekday(Map<Menu, Integer> menusAndCounts, List<Menu> menus, int dateOfVisit) {
+        if (!December.containDayInWeekday(dateOfVisit))
+            return;
+
+        int dessertMenuNumber = countCourseMenu(menusAndCounts, menus, Menu.Course.DESSERT);
+        if (dessertMenuNumber == 0) return;
+
+        int weekdayDiscountPrice = dessertMenuNumber * 2023;
+        totalDiscountPrice += weekdayDiscountPrice;
+        String stringCommaWeekdayDiscountPrice = convertIntPriceToStringCommaPrice(weekdayDiscountPrice);
+        addDiscountPriceMessage("평일", stringCommaWeekdayDiscountPrice);
+    }
+
+    private void discountWeekend(Map<Menu, Integer> menusAndCounts, List<Menu> menus, int dateOfVisit) {
+        if (!December.containDayInWeekend(dateOfVisit))
+            return;
+
+        int mainMenuNumber = countCourseMenu(menusAndCounts, menus, Menu.Course.MAIN);
+        if (mainMenuNumber == 0) return;
+
+        int weekendDiscountPrice = mainMenuNumber * 2023;
+        totalDiscountPrice += weekendDiscountPrice;
+        String stringCommaWeekendDiscountPrice = convertIntPriceToStringCommaPrice(weekendDiscountPrice);
+        addDiscountPriceMessage("주말", stringCommaWeekendDiscountPrice);
+    }
+
+    private int countCourseMenu(Map<Menu, Integer> menusAndCounts, List<Menu> menus, Menu.Course course) {
+        int dessertMenuNumber = 0;
+
+        for (Menu menu : menus) {
+            if (menu.getCourse().equals(course))
+                dessertMenuNumber += menusAndCounts.get(menu);
+        }
+
+        return dessertMenuNumber;
+    }
+
+    private void addChristmasD_DayDiscountMessage(int discountPrice) {
+        if (discountPrice == 0)
+            return;
+        String stringCommaDiscountPrice = convertIntPriceToStringCommaPrice(discountPrice);
+        addDiscountPriceMessage("크리스마스 디데이", stringCommaDiscountPrice);
+    }
+
+    private void addDiscountPriceMessage(String discountType, String stringCommaDiscountPrice) {
+        discountDetailsMessage.add(discountType + " 할인: -" + stringCommaDiscountPrice + "원");
+    }
+
+    private int discountChristmasD_Day(int dateOfVisit) {
+        if (isOverDateChristmas(dateOfVisit))
+            return 0;
+
+        int discountPrice = 1000 + 100 * (dateOfVisit - 1);
+        totalDiscountPrice += discountPrice;
+        return discountPrice;
+    }
+
+    private boolean isOverDateChristmas(int dateOfVisit) {
+        return dateOfVisit > 25;
     }
 }
