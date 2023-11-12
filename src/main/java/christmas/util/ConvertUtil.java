b818ad822de3bceed6de7cedd40a210df6cafeee
WooYoungDoo <duden5000@naver.com>
2023-11-12 15:10:35 +0900

    feat (ConvertUtil): 문자열 -> 숫자 변환 로직

--- src/main/java/christmas/util/ConvertUtil.java
+++ src/main/java/christmas/util/ConvertUtil.java
@@ -5,4 +5,8 @@ public class ConvertUtil {
     public static String convertIntPriceToStringCommaPrice(int intPrice) {
         return String.format("%,d", intPrice);
     }
+
+    public static int convertStringToInt(String inputString) {
+        return Integer.parseInt(inputString);
+    }
 }
