da561a24c32b0d09f7f0d69fd729be05c8aaf92f
WooYoungDoo <duden5000@naver.com>
2023-11-12 21:31:11 +0900

    comment (SeparationUtil) : 각 매서드 필요 설명 주석

--- src/main/java/christmas/util/SeparationUtil.java
+++ src/main/java/christmas/util/SeparationUtil.java
@@ -8,11 +8,11 @@ public class SeparationUtil {
     private static final String COMMA_SPLIT = ",";
     private static final String MINUS_SIGN_SPLIT = "-";
 
-    public static List<String> separateMenus(String inputWinningNumber) {
+    public static List<String> separateMenus(String inputWinningNumber) {   // 리턴 값 ex) [해산물파스타-2,초코케이크-1]
         return Stream.of(inputWinningNumber.split(COMMA_SPLIT)).toList();
     }
 
-    public static List<String> separateMenusAndCount(String menus) {
+    public static List<String> separateMenusAndCount(String menus) {    // 리턴 값 ex) [해산물파스타,2]
         return Stream.of(menus.split(MINUS_SIGN_SPLIT)).toList();
     }
 }
