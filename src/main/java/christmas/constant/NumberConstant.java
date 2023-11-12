325dc43e424f173892a60db5ce05b4915f5b211e
WooYoungDoo <duden5000@naver.com>
2023-11-12 21:32:23 +0900

    feat (NumberConstant) : 숫자 관련 상수 클래스

---
+++ src/main/java/christmas/constant/NumberConstant.java
@@ -0,0 +1,14 @@
+package christmas.constant;
+
+public class NumberConstant {
+
+    public static final int ZERO_VALUE = 0;
+    public static final int ONE_VALUE = 1;
+    public static final int CHRISTMAS_DAY = 25;
+    public static final int HUNDRED_VALUE = 100;
+    public static final int THOUSAND_VALUE = 1000;
+    public static final int STAR_DAY_DISCOUNT_PRICE = 1000;
+    public static final int THIS_YEAR_NUMBER_VALUE = 2023;
+    public static final int CHAMPAGNE_PRICE = 25000;
+    public static final int BASED_ON_OVER_PRICE_GIVE_GIFT_MENU = 120_000;
+}
